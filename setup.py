c27bc365d978e0da3351df8f5b316b5f6cbc3d0a
kdbasema <kdbasema@iastate.edu>
2017-07-26 11:01:54 -0400

    Added temporary test file and reorganized

---
+++ setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='cool_project',
+        version="0.1.1",
+        description='SCF program construction',
+        author='Kevin Basemann',
+        author_email='kdbasema@iastate.edu',
+        url="https://github.com/kdbasema/QM_2017_SSS_Team4",
+        license='BSD-3C',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'numpy>=1.7',
+        ],
+        extras_require={
+            'docs': [
+                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+                'tox',
+            ],
+        },
+
+        tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+            'tox',
+        ],
+
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+        ],
+        zip_safe=True,
+    )
+
---
+++ tests/test_tmp.py
@@ -0,0 +1,4 @@
+import pytest
+
+def tmp():
+    0 == 0
